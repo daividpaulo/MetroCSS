013d17bbef137f503a5252f60f66d129930b822e
DAIVID PAULO V F DE SOUSA <daivid.paulo@unimedcaruaru.com.br>
2013-12-12 16:22:25 -0300

    tudo

---
+++ Projeto Metro/Gerenciador/MvcApplication1/Controllers/PrincipalController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        //
+        // GET: /Principal/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PartialTeste()
+        {
+            return PartialView("_PartialTeste");
+        }
+
+        public ActionResult Inicio()
+        {
+
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            return View();
+        }
+
+    }
+}
